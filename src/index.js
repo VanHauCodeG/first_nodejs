3a0be4b02944273102f749f4636c58437c65b3be
VanHauCodeG <hau551022@gmail.com>
2026-09-11 19:25:23 +0700

    remove public/css folder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-//HTTPP log
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP log
 app.use(morgan('combined'))
 
 //Template engine Hadlebars 
@@ -25,6 +27,7 @@ app.get('/news', (req, res) => {
   res.render('news');
 });
 
+//Status
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
