a3e17813d4a26909efb08c0f70ed66112dd43746
VanHauCodeG <hau551022@gmail.com>
2026-09-15 20:40:00 +0700

    feat: connect to database with mongodb

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,14 @@
+const Course = require ('../models/Courses');
+
 class SiteControllers{
     // [GET] /
-    index(req, res) {
-        res.render('home');
+    async index(req, res, next) {
+        try {
+            const courses = await Course.find({});
+            res.json(courses);
+        } catch (err) {
+            next(err);
+        }
     }
 
     //[GET] /search
